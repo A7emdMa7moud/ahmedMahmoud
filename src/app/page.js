d911e0c1b8700d3f59d01ceddf35a631dccb5c54
Ahmed Mahmoud <153916812+A7emdMa7moud@users.noreply.github.com>
2025-02-17 16:37:37 +0200

    a lot of updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
 "use client";
 
-import ahmed from "../app/_photos/ahmedMahmoud.png";
-import Image from "next/image";
+// import ahmed from "../app/_photos/ahmedMahmoud.png";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="home">
       <div className="border-2 rounded-lg w-[100%] relative overflow-hidden border-line">
         <span className="homeStyle"></span>
-        <Image
+        {/* <Image
           className="animate__animated animate__slideInRight"
           src={ahmed}
           width={"100%"}
           alt="Ahmed Mahmoud"
-        />
+        /> */}
       </div>
     </div>
   );
